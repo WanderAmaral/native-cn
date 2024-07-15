b86b29db2e9b7d9d6f93354a8f935679a3ed3a48
Wander Amaral <wanderamaral013@gmail.com>
2024-07-15 15:07:06 -0300

    feat: create project and chore: setup tailwind

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import '@/styles/global.css'
+
+import { StatusBar } from "expo-status-bar";
+
+import Profile from "@/app/Profile";
+
+export default function App() {
+  return (
+    <>
+      <Profile />
+      <StatusBar style="auto" />
+    </>
+  );
+}
---
+++ src/app/Profile.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Profile() {
+  return (
+    <View className="flex items-center justify-center h-full">
+      <Text className="text-3xl font-bold font-sans text-red-500">Hello world</Text>
+    </View>
+  );
+}
